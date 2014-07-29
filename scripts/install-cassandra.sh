7126fe7421cc88ba68eddacaed0903f461802c1c
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2014-07-29 05:20:36 +0200

    catch more default settings when changing Xss

--- scripts/install-cassandra.sh
+++ scripts/install-cassandra.sh
@@ -4,6 +4,6 @@ CASSANDRA_VERSION=$1
 
 wget https://archive.apache.org/dist/cassandra/$CASSANDRA_VERSION/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
 tar xfz apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz && rm apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz
-sed -i 's/Xss180k/Xss256k/g' apache-cassandra-$CASSANDRA_VERSION/conf/cassandra-env.sh
+sed -i 's/Xss1..k/Xss256k/g' apache-cassandra-$CASSANDRA_VERSION/conf/cassandra-env.sh
 mv apache-cassandra-$CASSANDRA_VERSION /usr/local/
 ln -sf /usr/local/apache-cassandra-$CASSANDRA_VERSION /usr/local/apache-cassandra
