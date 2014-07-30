d9be67d1558f00c1e6e1064d9b07d8a4fa36fb78
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2014-07-30 02:10:02 +0200

    changed order of ES version check

--- utils/get-elasticsearch-version.sh
+++ utils/get-elasticsearch-version.sh
@@ -4,11 +4,11 @@ TITAN_VERSION=$1
 
 if [ -z $ELASTICSEARCH_VERSION ]; then
   TITAN_VERSION=`./get-titan-version.sh $TITAN_VERSION`
-  ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/titan-es/pom.xml | grep -Po '(?<=<elasticsearch.version>).*(?=</elasticsearch.version>)'`
+  ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/pom.xml | grep -Po '(?<=<elasticsearch.version>).*(?=</elasticsearch.version>)'`
   if [ -z $ELASTICSEARCH_VERSION ]; then
-    ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/titan-es/pom.xml | grep -A8 '<artifactId>elasticsearch</artifactId>' | grep -Po '(?<=<version>).*(?=</version>)'`
+    ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/titan-es/pom.xml | grep -Po '(?<=<elasticsearch.version>).*(?=</elasticsearch.version>)'`
     if [ -z $ELASTICSEARCH_VERSION ]; then
-      ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/pom.xml | grep -Po '(?<=<elasticsearch.version>).*(?=</elasticsearch.version>)'`
+      ELASTICSEARCH_VERSION=`curl -s https://raw.githubusercontent.com/thinkaurelius/titan/$TITAN_VERSION/titan-es/pom.xml | grep -A8 '<artifactId>elasticsearch</artifactId>' | grep -Po '(?<=<version>).*(?=</version>)'`
     fi
   fi
 fi
